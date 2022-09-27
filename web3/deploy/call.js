593f386b02cec87e77db2e7e882dcf1f7e024465
hugomatic <hugo@hugomatic.ca>
2022-09-27 13:16:44 -0700

    split events out of call

--- web3/deploy/call.js
+++ web3/deploy/call.js
@@ -5,20 +5,19 @@ const dotenv = require('dotenv')
 // load contract abi
 
 
-async function call_method(contract_json, contract_address, method, methodParam) {
-
-  const { abi } = JSON.parse(fs.readFileSync(contract_json));
-
+async function call_method(contract_abi, contract_address, method, methodParam) {
   const network = process.env.POLYGON_NETWORK
   const web3 = new Web3 (
     new Web3.providers.HttpProvider(
-      `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`))
+      `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`)
+  )
+
+  let txUrl = ""
+  const contract = new web3.eth.Contract( contract_abi, contract_address)
 
   const signer = web3.eth.accounts.privateKeyToAccount(process.env.SIGNER_PRIVATE_KEY)
   web3.eth.accounts.wallet.add(signer)
 
-  let txUrl = ""
-  const contract = new web3.eth.Contract( abi, contract_address)
   const tx = contract.methods.echo(methodParam)
   const receipt = await tx.send({
       from: signer.address,
@@ -28,27 +27,38 @@ async function call_method(contract_json, contract_address, method, methodParam)
       console.log(`Mining transaction ...`)
       const chain = network.replace('polygon-', '')
       txUrl = `https://${chain}.polygonscan.com/tx/${txhash}`
-      console.log(txUrl)
-      console.log
+      // console.log(txUrl)
     })
   // done
   console.log(`Mined in block ${receipt.blockNumber}`)
 
-  console.log('\nEvents:')
-  const opts ={fromBlock: receipt.blockNumber-3000, toBlock: 'latest', filter: {}}
-  const past_events = await contract.getPastEvents('Echo', opts)
-  console.log(past_events)
-
+  // console.log(past_events)
   return {
     block: receipt.blockNumber,
     url: txUrl,
-    events: past_events
+    receipt: receipt
   }
 
   //  .then(results => console.log(results))
   //  .catch(err => {throw err})
 }
 
+async function get_events(contract_abi, contract_address, event_name) {
+
+  console.log('\nEvents:')
+  const network = process.env.POLYGON_NETWORK
+  const web3 = new Web3 (
+    new Web3.providers.HttpProvider(
+      `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`)
+  )
+
+  const contract = new web3.eth.Contract( contract_abi, contract_address)
+  const blockFrom = await web3.eth.getBlockNumber() - 3000
+  const opts ={fromBlock: blockFrom, toBlock: 'latest', filter: {}}
+  const past_events = await contract.getPastEvents(event_name, opts)
+
+  return past_events
+}
 
 dotenv.config()
 // main()
@@ -60,8 +70,22 @@ async function main() {
   method = process.argv[4]
   param = process.argv[5]
 
-  await call_method(contract_json, contract_address, method, param)
+  const { abi } = JSON.parse(fs.readFileSync(contract_json));
+  results = await call_method(abi, contract_address, method, param)
+
+  console.log('Call results:')
+  console.log(` - mined in block ${results.block}`)
+  console.log(` - url: ${results.url}`)
+
+  const past_events = await get_events(abi, contract_address, 'Echo')
+  console.log(past_events)
+  for (i in past_events) {
+    const ev = past_events[i]
+    const res = JSON.stringify(ev.returnValues)
+    console.log(`    - ${i} Event: ${res}`)
+  }
 }
+
 
 if (typeof require !== 'undefined' && require.main === module) {
   // deploy_contract(contract_json).then(() => process.exit(0))
