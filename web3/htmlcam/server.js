fbf0d32bee3bed69768d9824028f4b224d3b1939
Vineet <Vineetk.ks@gmail.com>
2022-10-17 17:08:00 -0700

    working with minimal text output

--- web3/htmlcam/server.js
+++ web3/htmlcam/server.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import fs from 'fs'
 import formidable from 'formidable'
-
+import dotenv from 'dotenv'
+import vision from '@google-cloud/vision'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+dotenv.config()
 
 const app = express()
 const port = 4444
@@ -30,10 +35,15 @@ app.post('/photo', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' })
     const tmpPath = files.image[0].filepath
     console.log(tmpPath)
-    fs.rename(tmpPath, imagePath, ()=>{
+    fs.rename(tmpPath, imagePath, async ()=>{
       console.log('photo saved to', imagePath)
-      res.end(JSON.stringify({ fields, files }, null, 2));
-
+      // res.end(JSON.stringify({ fields, files }, null, 2));
+      // Performs text detection on the local file
+      const [result] = await client.textDetection(imagePath)
+      const detections = result.textAnnotations;
+      console.log('Text:');
+      detections.forEach(text => console.log(text));
+      res.end(JSON.stringify(detections[0].description, null, 2))
     })
 
   });
