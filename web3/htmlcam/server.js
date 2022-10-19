ab8f5a7e7005397c7fef587bc24976b4fb4b1dc4
hugomatic <hugo@hugomatic.ca>
2022-10-18 18:02:37 -0700

    parsing the google ocr string without crashing all the time

--- web3/htmlcam/server.js
+++ web3/htmlcam/server.js
@@ -22,6 +22,59 @@ console.log('SAVING to ', imagePath)
 
 app.use(express.static('public'))
 
+function parse_ticket_ocr_string(ocrStr) {
+  const lines = ocrStr.split('\\n')
+
+  console.log('\n','parse')
+  for (let i in lines) {
+    console.log(i, lines[i])
+  }
+  let id = ""
+  let price = 0
+
+  const valid = lines[1].replace("VALID FOR", "")
+  let startZone = 0
+  let endZone = 0
+  for (let i in lines) {
+    const l = lines[i]
+
+    // id and price
+    if (l.length > 25 && l.indexOf('$') > 20) {
+      const x = l.split('$')
+      const idStr = x[0].trim()
+      let priceStr = x[1]
+      if (!priceStr) {
+        priceStr = ''
+      }
+      priceStr = priceStr.replace('*','')
+      priceStr = priceStr.replace('"','')
+      priceStr = priceStr.replace('\\','')
+      price = Number(priceStr.trim())
+      id = idStr
+    }
+
+    // start / end zone
+    if (l.length == 1) {
+      if (startZone == 0 ) {
+        startZone = Number(l)
+      }
+      else {
+        if (endZone == 0) {
+          endZone = Number(l)
+        }
+      }
+    }
+  }
+  return { ocr: ocrStr,
+           valid: valid.trim(),
+           startZone: startZone,
+           endZone: endZone,
+           ticketId: id,
+           price: price,
+           // pos: lines[8]
+         }
+}
+
 app.post('/photo', (req, res) => {
 
   const form = formidable({ multiples: true });
@@ -32,7 +85,6 @@ app.post('/photo', (req, res) => {
       res.end(String(err));
       return;
     }
-    res.writeHead(200, { 'Content-Type': 'application/json' })
     const tmpPath = files.image[0].filepath
     console.log(tmpPath)
     fs.rename(tmpPath, imagePath, async ()=>{
@@ -41,9 +93,13 @@ app.post('/photo', (req, res) => {
       // Performs text detection on the local file
       const [result] = await client.textDetection(imagePath)
       const detections = result.textAnnotations;
-      console.log('Text:');
-      detections.forEach(text => console.log(text));
-      res.end(JSON.stringify(detections[0].description, null, 2))
+      const text = JSON.stringify(detections[0].description, null,2)
+      console.log('ocr output',text)
+      const ticketData = parse_ticket_ocr_string(text)
+      // console.log(JSON.stringify(detections, null, 2));
+      console.log('ticket data',JSON.stringify(ticketData))
+      // res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.json(ticketData)
     })
 
   });
